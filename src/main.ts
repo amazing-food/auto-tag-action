b83e49038e314d00f3757361ce74e1209c032af5
Eli <eli@amazing.co.il>
2021-12-14 14:39:08 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+async function run(): Promise<void> {
+  try {
+    const token = core.getInput('token', {required: true})
+    const name = core.getInput('name')
+    const git = github.getOctokit(token)
+    const tags = await git.rest.repos.listTags({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      per_page: 10
+    })
+
+    const map = tags.data
+      .filter(s => !isNaN(parseInt(s.name)))
+      .sort((a, b) => parseInt(a.name) - parseInt(b.name))
+    const latest = map[0].commit.sha
+    const commits = await git.rest.repos.compareCommits({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      base: latest,
+      head: 'master'
+    })
+
+    await git.rest.repos.createRelease({
+      name: `Auto release ${name}`,
+      draft: false,
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      tag_name: name,
+      body: commits.data.commits.map(c => c.commit.message).join('\n')
+    })
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(`Process failed with ${error.message}`)
+    }
+  }
+}
+
+run()
