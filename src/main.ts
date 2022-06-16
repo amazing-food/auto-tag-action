5fff7ef26ad3ee49e64e0505d8a07d633d054ddd
Eli Y <eli@amazing.co.il>
2022-06-16 11:59:00 +0300

    Updating dependencies Switching to auto-generated changelog

--- src/main.ts
+++ src/main.ts
@@ -6,33 +6,14 @@ async function run(): Promise<void> {
     const token = core.getInput('token', {required: true})
     const name = core.getInput('name')
     const git = github.getOctokit(token)
-    const tags = await git.rest.repos.listTags({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      per_page: 10
-    })
-
-    const map = tags.data
-      .filter(s => !isNaN(parseInt(s.name)))
-      .sort((a, b) => parseInt(b.name) - parseInt(a.name))
-    const latest = map[0].commit.sha
-    const commits = await git.rest.repos.compareCommits({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      base: latest,
-      head: 'master'
-    })
 
     await git.rest.repos.createRelease({
       name: `Auto release ${name}`,
       draft: false,
+      generate_release_notes: true,
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      tag_name: name,
-      body: commits.data.commits
-        .filter(c => c.commit.committer?.email === 'noreply@github.com')
-        .map(c => `* ${c.sha} ${c.commit.message}`)
-        .join('\n')
+      tag_name: name
     })
   } catch (error) {
     if (error instanceof Error) {
